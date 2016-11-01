363ae5a45aba4a724ceb61298b9a82a9345b317e
aledaniel <ale.daniel00@yahoo.es>
2016-11-01 20:50:29 -0300

    closeModal

---
+++ VersionBeta/js/components/contenedor-principal/contenedor-principal.js
@@ -0,0 +1,4 @@
+angular.module('turnosApp').
+component('contenedorPrincipal', {
+    templateUrl: './js/components/contenedor-principal/contenedor-principal.html'
+});
---
+++ VersionBeta/js/controllers/controller.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+    $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
+    $('#modal1').openModal();
+    $('#modal1').closeModal();
+});
